70af5db289c43d1c62db48570c3c78f37f03d2a7
WildeMedia <wildemedia33@gmail.com>
2020-04-10 15:34:49 +0100

    Added the logic which allows a user to update their username.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,27 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
   e.preventDefault();
   const message = newChatForm.message.value.trim();
+  // update message via chatroom Class
   chatroom.addChat(message)
+    //reset the form when promise is resolved
     .then(() => newChatForm.reset())
     .catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom Class
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName)
+    //reset the form 
+     newNameForm.reset();
 });
 
 // class instances

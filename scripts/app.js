09b945dd16ab303d0c3e4902d51ff308c2cdb57e
WildeMedia <wildemedia33@gmail.com>
2020-04-11 10:22:49 +0100

    Wrote the logic to save the last user to local storage. if there is no user they will be given the name of anon, until they add a user name.

--- scripts/app.js
+++ scripts/app.js
@@ -28,9 +28,12 @@ newNameForm.addEventListener('submit', e => {
     setTimeout(() => updateMssg.innerText = '', 3000);
 });
 
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';
+
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'shaun');
+const chatroom = new Chatroom('gaming', username);
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
